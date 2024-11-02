2874f6ca51b63dc20b091dfb3cc05356fcbea2ec
shoppingmall_toyproject <hanjung25@naver.com>
2024-11-02 11:22:52 +0900

    algorithm241102 update:

---
+++ src/ETC/four/main.java
@@ -0,0 +1,29 @@
+package ETC.four;
+
+import java.util.Scanner;
+
+public class main {
+
+    public static void main(String[] args) {
+
+        int inputNum = 19;
+        int bin[] = new int[100]; //이진수를 저장할 배열
+
+        int i = 0; //bin 배열의 index카운팅
+        int mok = inputNum;
+
+        while(mok > 0) {
+
+            bin[i] = mok % 2;
+            mok /=2;
+            i++;
+
+        }
+        i--;
+        for(; i>=0; i--){
+            System.out.print(bin[i]);
+        }
+
+    }
+
+}
