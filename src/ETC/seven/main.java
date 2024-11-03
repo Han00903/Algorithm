2f7cbae6c943e739f55a6f930e492bee51e4c5cc
shoppingmall_toyproject <hanjung25@naver.com>
2024-11-03 11:54:57 +0900

    알고리즘 241103

---
+++ src/ETC/seven/main.java
@@ -0,0 +1,23 @@
+package ETC.seven;
+
+public class main {
+
+    public static void main(String[] args) {
+
+        int num = 13;
+        boolean isPrimeNumber = true;
+
+        for (int i = 2; i<= num -1; i++) {
+            if(num%i==0) {
+                isPrimeNumber = false;
+            }
+        }
+
+        if(isPrimeNumber) {
+            System.out.println(num + ": 소수입니다.");
+        }else{
+            System.out.println(num + ": 소수가 아닙니다.");
+        }
+
+    }
+}
