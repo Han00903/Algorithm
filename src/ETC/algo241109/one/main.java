2950f2dd2be46018658cb4b7e4d1a5f31255b88f
Han00903 <hanjung25@naver.com>
2024-11-09 15:47:58 +0900

    algo241109

---
+++ src/ETC/algo241109/one/main.java
@@ -0,0 +1,19 @@
+package ETC.algo241109.one;
+
+public class main {
+    public static void main(String[] args){
+
+        int n = 5;
+
+        for(int i=0; i<n; i++){
+            for(int j=0; j<n; j++){
+                if(n-i-j<=1){
+                    System.out.print("*");
+                }else {
+                    System.out.print(" ");
+                }
+            }
+            System.out.println();
+        }
+    }
+}
