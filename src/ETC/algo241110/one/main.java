2f0a97f4b37adad3638ea456416e44b8fadff065
Han00903 <hanjung25@naver.com>
2024-11-11 20:11:54 +0900

    algo241110

---
+++ src/ETC/algo241110/one/main.java
@@ -0,0 +1,31 @@
+package ETC.algo241110.one;
+
+public class main {
+
+    public static int getNum (int i){
+        int j = 0;
+
+        while(i>0){
+            if(i%10==3||i%10==6||i%10==9)
+                j++;
+            i /= 10;
+        }
+        return  j;
+    }
+    public static void main(String[] args){
+
+        int n = 100;
+
+        for(int i=1; i<=n; i++){
+            int cnt = getNum(i);
+            if(cnt == 0){
+                System.out.print(i+" ");
+            }else {
+                for(int l =0; l<cnt; l++){
+                    System.out.print("짝");
+                }
+                System.out.print(" ");
+            }
+        }
+    }
+}
---
+++ src/ETC/algo241110/two/main.java
@@ -0,0 +1,4 @@
+package ETC.algo241110.two;
+
+public class main {
+}
