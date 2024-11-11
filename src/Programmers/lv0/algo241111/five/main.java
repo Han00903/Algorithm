d7c8b6f7c9c6c8d04cebcc5b12f9ed16ba6c6916
Han00903 <hanjung25@naver.com>
2024-11-11 23:47:52 +0900

    프로그래머스 lv0 241111

---
+++ src/Programmers/lv0/algo241111/five/main.java
@@ -0,0 +1,14 @@
+package Programmers.lv0.algo241111.five;
+// 숫자 비교하기
+public class main {
+    public int solution(int num1, int num2) {
+        int answer = 0;
+
+        if(num1==num2){
+            answer = 1;
+        }else{
+            answer = -1;
+        }
+        return answer;
+    }
+}
