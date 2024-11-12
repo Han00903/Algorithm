61b27bf65056e2ea6a3ee33b17036fc1e4cdbe09
Han00903 <hanjung25@naver.com>
2024-11-12 10:19:39 +0900

    프로그래머스 lv1 241112

---
+++ src/Programmers/lv1/algo241112/one/main.java
@@ -0,0 +1,14 @@
+package Programmers.lv1.algo241112.one;
+
+public class main {
+    public int solution(int n) {
+
+        int sum = 0;
+        while(n>0){
+            int m = n%10;
+            sum += m;
+            n /= 10;
+        }
+        return sum;
+    }
+}
