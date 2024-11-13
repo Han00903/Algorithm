1c26dad2ed7117cc01c1cfe6e30f92a2b6fc3b78
Han00903 <hanjung25@naver.com>
2024-11-13 10:18:52 +0900

    프로그래머스 lv1 241113

---
+++ src/Programmers/lv1/algo241113/one/main.java
@@ -0,0 +1,14 @@
+package Programmers.lv1.algo241113.one;
+
+public class main {
+    public long[] solution(int x, int n) {
+
+        long[] answer = new long[n];
+
+        for(int i = 0; i<n; i++){
+            answer[i] = (long)x *(i+1);
+        }
+
+        return answer;
+    }
+}
---
+++ src/Programmers/lv1/algo241113/two/main.java
@@ -0,0 +1,13 @@
+package Programmers.lv1.algo241113.two;
+
+public class main {
+    public int solution(int n) {
+
+        for(int i=1; i<n; i++){
+            if(n%i ==1){
+                return i;
+            }
+        }
+        return -1;
+    }
+}
