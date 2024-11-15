a60a38cacd780820dbe931d0f9bc4901d59c9ca7
Han00903 <hanjung25@naver.com>
2024-11-15 21:17:13 +0900

    백준 241115:

---
+++ src/Baekjoon/Silver/algo241115/BOJ9012/Main.java
@@ -0,0 +1,45 @@
+package Baekjoon.Silver.algo241115.BOJ9012;
+
+import java.util.Scanner;
+
+public class Main {
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        int T = sc.nextInt(); // 테스트 데이터 개수
+        sc.nextLine(); // 버퍼 비우기
+
+        for (int i = 0; i < T; i++) {
+            String data = sc.nextLine();
+
+            if (isVPS(data)) {
+                System.out.println("YES");
+            } else {
+                System.out.println("NO");
+            }
+        }
+
+        sc.close();
+    }
+
+    private static boolean isVPS(String data) {
+        int balance = 0; // 괄호의 균형을 나타냄
+
+        for (char ch : data.toCharArray()) {
+            if (ch == '(') {
+                balance++;
+            } else if (ch == ')') {
+                balance--;
+            }
+
+            // 닫는 괄호가 더 많아지면 VPS가 될 수 없음
+            if (balance < 0) {
+                return false;
+            }
+        }
+
+        // 모든 괄호를 처리한 후 균형이 맞아야 함
+        return balance == 0;
+    }
+}
