48aadf831e2beda4458875fb711f35db20e2f13e
Han00903 <145526597+Han00903@users.noreply.github.com>
2024-11-15 18:31:06 +0900

    Update Main.java

--- src/Baekjoon/Silver/algo241112/BOJ2839/Main.java
+++ src/Baekjoon/Silver/algo241112/BOJ2839/Main.java
@@ -1,26 +1,21 @@
 package Baekjoon.Silver.algo241112.BOJ2839;
 
-import java.util.Scanner;
-
-public class Main {
-
+public class Main{
     public static void main(String[] args){
         Scanner sc = new Scanner(System.in);
-
-        System.out.print("배달해야하는 설탕은 몇 킬로그램인가요?");
-
         int a = sc.nextInt();
-        System.out.print("오 "+a+"킬로그램이군요");
+        int count = 0;
 
-        int b = a/5;
-        int c = (a-5*b)/3;
-
-        if(a<5){
-            System.out.println(-1);
-        }else if((a-5*b)<3){
-            System.out.println(-1);
-        }else {
-            System.out.println(b+c);
+        while (a >= 0) {
+            if (a % 5 == 0) {
+                count += a / 5;
+                System.out.println(count);
+                return;
+            }
+            a -= 3;
+            count++;
         }
+        
+        System.out.println(-1);
     }
 }
