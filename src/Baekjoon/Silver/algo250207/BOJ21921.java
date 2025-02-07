a1d4eacd0fda2de6129282edb7a1df381a35e59a
Han00903 <hanjung25@naver.com>
2025-02-07 13:46:11 +0900

    출력초과로 인한 재작성

--- src/Baekjoon/Silver/algo250207/BOJ21921.java
+++ src/Baekjoon/Silver/algo250207/BOJ21921.java
@@ -1,47 +1,49 @@
-package Baekjoon.Silver.algo250207;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
 
-import java.util.Arrays;
-import java.util.Scanner;
 public class BOJ21921 {
-    public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 
-        int N = sc.nextInt(); // 전체 날짜 수
-        int X = sc.nextInt(); // 연속 날짜 수
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        int N = Integer.parseInt(st.nextToken());
+        int X = Integer.parseInt(st.nextToken());
 
-        int[] a = new int[N]; // 1일차부터 N일차까지 하루 방문자 수
-
+        int[] visitors = new int[N];
+        st = new StringTokenizer(br.readLine());
         for (int i = 0; i < N; i++) {
-            a[i] = sc.nextInt();
+            visitors[i] = Integer.parseInt(st.nextToken());
         }
-        System.out.println(Arrays.toString(a));
 
-        int sum =0;
-        int count =0; // 최대 방문자 수 합
+        int maxSum = 0, currentSum = 0, maxCount = 0;
 
         // 초기 윈도우 합 계산
-        for(int i =0; i<X; i++){
-            sum +=a[i];
+        for (int i = 0; i < X; i++) {
+            currentSum += visitors[i];
         }
 
-        int maxSum = sum;
+        maxSum = currentSum;
+        maxCount = 1;
 
         // 슬라이딩 윈도우 이동
-        for(int i=X; i<N; i++){
-            sum = sum - a[i-X] + a[i];
-            if(sum>maxSum){
-                maxSum = sum;
-                count =1;
-            }else if(sum == maxSum){
-                count++;
+        for (int i = X; i < N; i++) {
+            currentSum = currentSum - visitors[i - X] + visitors[i];
+
+            if (currentSum > maxSum) {
+                maxSum = currentSum;
+                maxCount = 1;
+            } else if (currentSum == maxSum) {
+                maxCount++;
             }
         }
 
-        if(maxSum == 0){
-            System.out.print("SAD");
-        }else{
+        if (maxSum == 0) {
+            System.out.println("SAD");
+        } else {
             System.out.println(maxSum);
-            System.out.println(count);
+            System.out.println(maxCount);
         }
     }
 }
