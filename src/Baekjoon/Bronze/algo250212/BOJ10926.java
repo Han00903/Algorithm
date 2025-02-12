224d8f7494142312c424202a66a8674e98d59296
Han00903 <hanjung25@naver.com>
2025-02-12 17:08:59 +0900

    algo250212

---
+++ src/Baekjoon/Bronze/algo250212/BOJ10926.java
@@ -0,0 +1,15 @@
+package Baekjoon.Bronze.algo250212;
+import java.util.*;
+public class BOJ10926 {
+
+    public static void main(String[] args){
+
+        Scanner sc = new Scanner(System.in);
+
+        String name = sc.next();
+        String emotion = "??!";
+
+        System.out.print(name+emotion);
+
+    }
+}
