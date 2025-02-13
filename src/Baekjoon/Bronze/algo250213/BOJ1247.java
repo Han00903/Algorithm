8e68731ce6e35da3aeea97b12b7fa2bb8343eceb
Han00903 <hanjung25@naver.com>
2025-02-13 12:40:58 +0900

    algo250213

---
+++ src/Baekjoon/Bronze/algo250213/BOJ1247.java
@@ -0,0 +1,37 @@
+package Baekjoon.Bronze.algo250213;
+
+import java.math.BigInteger;
+import java.util.Scanner;
+
+public class BOJ1247 {
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+        BigInteger[] SUM = new BigInteger[3];
+
+        for (int i = 0; i < 3; i++) {
+            SUM[i] = BigInteger.ZERO;
+        }
+
+        for (int i = 0; i < 3; i++) {
+            int N = sc.nextInt();
+
+            for (int j = 0; j < N; j++) {
+                BigInteger num = sc.nextBigInteger();
+                SUM[i] = SUM[i].add(num);
+            }
+        }
+
+        for (int k = 0; k < 3; k++) {
+            if (SUM[k].equals(BigInteger.ZERO)) {
+                System.out.println(0);
+            } else if (SUM[k].compareTo(BigInteger.ZERO) > 0) {
+                System.out.println("+");
+            } else {
+                System.out.println("-");
+            }
+        }
+
+        sc.close();
+    }
+}
