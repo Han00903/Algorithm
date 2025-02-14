fd8daa13af2f59ca1cffd7397c79a638a0dff6e5
Han00903 <hanjung25@naver.com>
2025-02-14 17:22:02 +0900

    algo250214

---
+++ src/Programmers/lv1/algo250214/Main12947.java
@@ -0,0 +1,15 @@
+package Programmers.lv1.algo250214;
+
+public class Main12947 {
+    public boolean solution(int x) {
+        int y = x;
+        int sum =0;
+
+        while(y>0){
+            sum +=y%10;
+            y /=10;
+        }
+
+        return x%sum==0;
+    }
+}
