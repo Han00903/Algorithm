a3c5ba2e29724a044829f53f3f396903c18e4a5a
Han00903 <hanjung25@naver.com>
2025-02-20 12:33:55 +0900

    algo250220

---
+++ src/Programmers/lv0/algo250220/Main181928.java
@@ -0,0 +1,23 @@
+package Programmers.lv0.algo250220;
+
+public class Main181928 {
+
+    public int solution(int[] num_list) {
+
+        StringBuilder a = new StringBuilder();
+        StringBuilder b = new StringBuilder();
+
+        for(int i : num_list){
+            if(i%2 == 0){
+                a.append(i);
+            }else if(i%2 != 0){
+                b.append(i);
+            }
+        }
+
+        int aNum = Integer.parseInt(a.toString());
+        int bNum = Integer.parseInt(b.toString());
+
+        return aNum + bNum;
+    }
+}
---
+++ src/Programmers/lv0/algo250220/Main181937.java
@@ -0,0 +1,11 @@
+package Programmers.lv0.algo250220;
+
+public class Main181937 {
+    public int solution(int num, int n) {
+        int answer = 0;
+
+        if(num%n ==0) answer =1;
+
+        return answer;
+    }
+}
